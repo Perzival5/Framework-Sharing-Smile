9ee131a51b7e16ad6903488643b3d0f06ff40068
David Gregori Rodriguez Calle <www.str_hpl@hotmail.com>
2025-10-20 18:17:28 -0400

    se configuro el entorno

---
+++ src/resources/users.py
@@ -0,0 +1,6 @@
+import config
+
+users = [
+    {"role": "admin", "username": config.ADMIN_USER, "password": config.ADMIN_PASS},
+    {"role": "professional", "username": config.PRO_USER, "password": config.PRO_PASS},
+]
